b0bf47a27d8e7199efc672a80c0bfcc38a8112e2
Lmntrix31 <92026387+Lmntrix31@users.noreply.github.com>
2024-01-17 20:23:09 +0100

    Update artifact path and site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://lmntrix31.github.io",
+  site: "https://lmntrix31.github.io/",
   integrations: [
     mdx({
       syntaxHighlight: 'shiki',
